dc44d4ae2bb9f36ee0c9531679e45dc430ec7119
Anderson Ventosilla <andersonventosilla0@gmai.com>
2022-04-08 00:59:13 -0500

    counter-finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
+import Boton from './componentes/Boton'
+import Contador from './componentes/Contador'
+import { useState } from 'react';
 
 function App() {
+
+  const [numClics,setNumClics] = useState(0)
+  /*
+    numClics => es el valor que queremos mostrar como el estado, este es el estado que vamos a guardar,usar y actualizar
+
+    setNumClics => es una funcion que nos va a permitir actualizarlo
+  */
+
+  const manejarClick = () => {
+    setNumClics(numClics + 1)
+  }
+
+  const reiniciarContador = () => {
+    setNumClics(0)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='freecodecamp-logo-contenedor'>
+        <img 
+          className='freecodecamp-logo'
+          src={freeCodeCampLogo}
+          alt='Logo de freecodecamp'
+        />
+      </div>
+      <div className='contenedor-principal'>
+        <Contador numClics={numClics} />
+        <Boton 
+          texto='Click'
+          esBotonDeClick = {true}
+/*este es un prop*/ manejarClick = {manejarClick}/*este es un function*/
+        />
+        <Boton 
+          texto = 'Reiniciar'
+          esBotonDeClick = {false}
+          manejarClick = {reiniciarContador}
+        />
+      </div>
     </div>
   );
 }
---
+++ src/componentes/Boton.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../css/boton.css'
+
+function Boton ({texto, esBotonDeClick, manejarClick}) {
+    return(
+        <button
+            className={esBotonDeClick ? 'boton-clic' : 'boton-reiniciar'}
+            onClick={manejarClick}
+        >
+            {texto}
+        </button>
+    )
+}
+
+export default Boton
